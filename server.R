b753a9312fb4dc69e7b518c745b22959c92d7215
JaroslavKotrba <65077222+JaroslavKotrba@users.noreply.github.com>
2023-06-23 13:22:20 +0200

    Update server.R

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 # server
+
 server <- function(input, output) {
-  options(scipen=999)
+  options(scipen = 999)
   make_result <- function(x, y, z) {as.numeric(x)*(1+ (-as.numeric(y)/(1+as.numeric(y))) )^as.numeric(z)}
   output$result <- renderText({paste0(round(make_result(input$select_amount, input$select_rate, input$select_year), 2), " CZK")
   })
@@ -19,35 +20,67 @@ server <- function(input, output) {
   })
   
   # CZECH INFLATION
+  
+  # Libraries
   library(rvest)
   library(dplyr)
   library(forecast)
+  library(zoo)
   library(tseries)
   library(curl)
+  library(lubridate)
+  library(data.table)
+  library(plotly)
+  library(ggplot2)
   
-  link <- "https://www.czso.cz/csu/czso/mira_inflace"
-  inf <- read_html(link)
+  load.data <- function(){
+    link <- "https://www.czso.cz/csu/czso/mira_inflace"
+    inf <- read_html(link)
+    
+    inf_load <- inf %>% html_nodes("table") %>% .[3] %>% 
+      html_table(fill = TRUE) %>% .[[1]]
+    
+    header.true <- function(df) { # move the first line to the title
+      names(df) <- as.character(unlist(df[1,]))
+      df[-c(1),]}
+    
+    inf_load <- data.frame(header.true(inf_load))
+    colnames(inf_load) <- c("year", "January", "February", "March",
+                            "April", "May", "June", "July", "August",
+                            "September", "October", "November", "December")
+    
+    inf_load$year <- NULL
+    
+    inf_vector <- as.vector(t(inf_load))
+    inf_vector <- as.numeric(gsub(",", ".", inf_vector))
+    
+    return(inf_vector)
+  }
   
-  inf_table <- inf %>% html_nodes("table") %>% .[3] %>% 
-    html_table(fill = TRUE) %>% .[[1]]
+  # inf_vector
+  inf_vector <- load.data()
   
-  header.true <- function(df) { #move the first line to the title
-    names(df) <- as.character(unlist(df[1,]))
-    df[-c(1),]}
+  transform.data <- function(inf_vector){
+    inf_table <- data.table(
+      date = seq(as.Date("1997-01-01"), as.Date(paste0(substr(Sys.Date() %m+% months(11), 1, 7), "-01")), by = "month")
+    )
+    
+    len <- length(inf_table$date)
+    inf_table$inflation <- inf_vector[1:len]
+    inf_table$forecast <- ifelse(is.na(inf_table$inflation), 1, 0)
+    
+    return(inf_table)
+  }
   
-  inf_table <- data.frame(header.true(inf_table))
-  colnames(inf_table) <- c("year", "January", "February", "March",
-                           "April", "May", "June", "July", "August",
-                           "September", "October", "November", "December")
+  # inf_table
+  inf_table <- transform.data(inf_vector)
   
-  inf_table$year <- NULL
-  
-  inf_vector <- as.vector(t(inf_table))
-  inf_vector <- as.numeric(gsub(",", ".", inf_vector))
-  
-  output$inflation <- renderPlot ({
-    Inflation <- ts(inf_vector, start = c(1997, 1),frequency = 12) # year month
-    plot(Inflation, lwd = 2, col = "red", main = "Inflation in the Czech Republic")
+  output$inflation <- renderPlotly ({
+    g <- ggplot(inf_table, aes(x=date, y=inflation)) +
+      geom_line(col='red', lwd=1) +
+      xlab("Date") +
+      ylab("Inflation") +
+      theme_bw()
   })
   
   output$current <- renderTable ({
